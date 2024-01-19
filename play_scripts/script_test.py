dd5fb8846f12110be31d61ddd2a6a6b23dc7cf97
milo <ucabm49@ucl.ac.uk>
2024-01-19 16:46:10 +0000

    setting the noise level for testing runs implimented

--- play_scripts/script_test.py
+++ play_scripts/script_test.py
@@ -3,9 +3,10 @@ import subprocess
 
 command = 'cd .. && python ./ase/run.py \
     --test --task A1ASE \
+    --noise_level 0.5 \
     --num_envs 1 \
-    --cfg_env ./ase/data/cfg/experiments/velocity_a1/a1_vel_est_env.yaml \
-    --cfg_train ./ase/data/cfg/experiments/velocity_a1/a1_vel_est_train.yaml \
+    --cfg_env ./ase/data/cfg/experiments/velocity_a1/noised_asym/a1_vel_est_env.yaml \
+    --cfg_train ./ase/data/cfg/experiments/velocity_a1//noised_asym/a1_vel_est_train.yaml \
     --motion_file ./ase/data/motions/dogo/dogo_data_1.yaml \
     --checkpoint ./output/ase_a1_OFFICIAL_asymetric_17-13-31-28/nn/ase_a1_OFFICIAL_asymetric.pth'
 
