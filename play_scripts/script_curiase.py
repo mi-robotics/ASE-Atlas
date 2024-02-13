192f3510685f9eba955c6f861204c8b9393e2e27
Milo Carroll <ucabm49@ucl.ac.uk>
2024-02-13 08:32:10 +0000

    curiase

---
+++ play_scripts/script_curiase.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+command = 'cd .. && python ./ase/run.py \
+    --test --task A1ASE \
+    --noise_level 1.0 \
+    --friction_overide 1 \
+    --use_delay 0 \
+    --num_envs 1 \
+    --cfg_env ./ase/data/cfg/experiments/curiase/test1/env.yaml \
+    --cfg_train ./ase/data/cfg/experiments/curiase/test1/train.yaml \
+    --motion_file ./ase/data/motions/dogo/dogo_data_1.yaml \
+    --checkpoint ./output/CURIASE_TEST_1_12-17-41-01/nn/CURIASE_TEST_1.pth'
+
+
+
+subprocess.run(command, shell=True, check=True, text=True)
